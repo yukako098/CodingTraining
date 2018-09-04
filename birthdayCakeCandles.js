29b99f2c9838eca21ed5704b7ef1421e75bd8439
yukako098 <yukakosunabe@gmail.com>
2018-09-03 22:27:50 -0700

    add birthdayCakeCandles.js and solveMeFirst.js

---
+++ birthdayCakeCandles.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthdayCakeCandles function below.
+function birthdayCakeCandles(ar) {
+    var count = 0;
+    for(var i = 0; i < ar.length; i++){
+        if(ar[i] == Math.max.apply(null,ar)){
+            count++;
+        }
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = birthdayCakeCandles(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
